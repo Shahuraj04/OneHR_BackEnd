3bbe98716a396df1f91cc17fd843fb85bd70c354
Shahuraj04 <shahurajbhoite04@gmail.com>
2026-02-11 18:37:25 +0530

    Security changed

--- src/main/java/com/proj/security/SecurityConfiguration.java
+++ src/main/java/com/proj/security/SecurityConfiguration.java
@@ -52,8 +52,11 @@ public class SecurityConfiguration {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList(
-            "http://localhost:5173", "http://127.0.0.1:5173",
-            "http://localhost:5174", "http://127.0.0.1:5174"
+             "http://localhost:5173",
+            "http://127.0.0.1:5173",
+            "http://localhost:5174",
+            "http://127.0.0.1:5174",
+            "https://onehr360.netlify.app"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
