41360f5502a12d1d4e84d53fb881580a9dae326c
Shahuraj04 <shahurajbhoite04@gmail.com>
2026-02-15 12:34:14 +0530

    logger added again

--- src/main/java/com/proj/config/UserHandshakeHandler.java
+++ src/main/java/com/proj/config/UserHandshakeHandler.java
@@ -7,8 +7,6 @@ import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
-import com.sun.security.auth.UserPrincipal;
-
 public class UserHandshakeHandler extends DefaultHandshakeHandler {
     @Override
     protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler, Map<String, Object> attributes) {
--- src/main/java/com/proj/config/WebSocketConfig.java
+++ src/main/java/com/proj/config/WebSocketConfig.java
@@ -14,7 +14,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setAllowedOrigins("http://localhost:3000", "http://localhost:5173")
+                .setAllowedOriginPatterns("*")
                 .setHandshakeHandler(new UserHandshakeHandler())
                 .withSockJS();
     }
